682ecfbfff3d1c70d4f03ad20dcc9564e7d5dc59
Дмитрий Вожегов <vodimitriy@hotmail.com>
2020-06-13 01:49:21 +0300

    Добавьте файлы проекта.

---
+++ WebServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace WebServer
+{
+    class Server
+    {
+        TcpListener Listener; // Объект, прослушивающий подключения по TCP
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            // Создаем "слушателя" для указанного порта
+            Listener = new TcpListener(IPAddress.Any, Port); // IPAddress.Any - сервер прослушивает подключения по всем интерфейсам
+            Listener.Start(); // Запуск сервера
+
+            // В бесконечном цикле
+            while (true)
+            {
+                // Принимаем новых клиентов
+                new Client(Listener.AcceptTcpClient());
+
+            }
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Создадим новый сервер на порту 80
+            new Server(5980);
+        }
+    }
+
+    class Client
+    {
+        // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
+        public Client(TcpClient Client)
+        {
+            // Код простой HTML-странички
+            string Html = "<html><body><h1>It works!</h1></body></html>";
+            // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+            string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            // Приведем строку к виду массива байт
+            byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+            // Отправим его клиенту
+
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            // Отправим его клиенту
+
+            Console.WriteLine("Connect client");
+
+            // Закроем соединение
+
+            Client.Close();
+
+        }
+    }
+}
---
+++ WebServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("WebServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebServer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0db4d076-5844-4681-ae81-7762d95d1e75")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
